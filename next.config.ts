ba71c2eb01c142f7344dd9a66293ee9ac55b17a8
D. Potikanond <potikanond@gmail.com>
2024-12-26 14:04:28 +0700

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+      CMU_ENTRAID_URL: process.env.CMU_ENTRAID_URL,
+      CMU_ENTRAID_GET_TOKEN_URL: process.env.CMU_ENTRAID_GET_TOKEN_URL,
+      CMU_ENTRAID_GET_BASIC_INFO: process.env.CMU_ENTRAID_GET_BASIC_INFO,
+      CMU_ENTRAID_LOGOUT_URL: process.env.CMU_ENTRAID_LOGOUT_URL,
+    },
+};
+
